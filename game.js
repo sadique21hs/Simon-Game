92596e178c888b5a6c18e3a3c632fe71daa71f70
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-08-31 00:31:34 +0600

    Add files via upload

---
+++ game.js
@@ -0,0 +1,11 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence() {
+  var randomnumber = Math.floor(Math.random() * 4);
+
+  var randomChosenColour = buttonColours[randomnumber];
+
+  gamePattern.push(randomChosenColour);
+}
