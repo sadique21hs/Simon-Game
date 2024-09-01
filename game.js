9d3026ba35b34d1fae5c01300ace63ce81ab2c98
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-09-02 00:40:28 +0600

    Edit and Updated

--- game.js
+++ game.js
@@ -1,50 +1,47 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
 
-$(".btn").click(function () {
-  var userChosenColour = $(this).attr("id");
-
-  userClickedPattern.push(userChosenColour);
-
-  playSound(userChosenColour);
-});
-
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
+var started = false;
 
 var level = 0;
 
 $(document).keypress(function () {
   if (!started) {
-    $("#level-title").text("Level" + level);
+    $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
 
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
 function nextSequence() {
-
   level++;
-  $("#level-title").text("Level" + level);
-  var randomnumber = Math.floor(Math.random() * 4);
 
-  var randomChosenColour = buttonColours[randomnumber];
+  $("#level-title").text("Level " + level);
 
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour)
     .fadeIn(100)
-    .fadeOut(100);
-
-  /* var audio = Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();*/
-
+    .fadeOut(100)
+    .fadeIn(100);
   playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
 function animatePress(currentColor) {
@@ -53,4 +50,3 @@ function animatePress(currentColor) {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
-
